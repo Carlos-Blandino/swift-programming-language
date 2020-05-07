ce63094b361756830a1dabf539c66e264385beac
Carlos Blandino <cablandino66@gmail.com>
2020-05-07 16:46:20 -0700

    finishing String indices examples, substrings, comparing strings, string and character equality ,and hasPrefix / hasSuffix method examples

--- strings-and-characters.playground/Contents.swift
+++ strings-and-characters.playground/Contents.swift
@@ -166,5 +166,118 @@ print(myWord.count)
 
 //startIndex and endIndex are equal when the string is empty
 
+var myGemanPhrase = "Ich gehe nach hause"
+//look at the index output from the following
+//see why we can't use the typical Integer index
+print(myGemanPhrase.startIndex)
+
+print(myGemanPhrase.endIndex)
+
+print(myGemanPhrase.index(before: myGemanPhrase.endIndex))
+
+print(myGemanPhrase.index(after: myGemanPhrase.startIndex))
+
+//lets see what those values are
+
+print(myGemanPhrase[myGemanPhrase.startIndex])
+
+//print(myGemanPhrase[myGemanPhrase.endIndex]) out of bounds error
+
+print(myGemanPhrase[myGemanPhrase.index(before: myGemanPhrase.endIndex)])
+
+print(myGemanPhrase[myGemanPhrase.index(after: myGemanPhrase.startIndex)])
+
+let specificIndex = myGemanPhrase.index(myGemanPhrase.startIndex, offsetBy: 5)
+var newString = "hellos"
+print(newString[specificIndex])
+//the specificIndex maps out to the 5 location on both but there is a difference
+//"e" in my word is missing the combining acute accent
+print(myWord[specificIndex])
+
+let specificIndex2 = myWord.index(myWord.startIndex, offsetBy: 5)
+print(myWord[specificIndex2])
+
+//insert at the beginning and make the word read in reverse
+var reverseResult: String = ""
+for index in myWord.indices {
+    reverseResult.insert(myWord[index], at: reverseResult.index(reverseResult.startIndex, offsetBy: 0))
+}
+print(reverseResult, " is nice")
+reverseResult = " good night"
+
+print(myWord)
+myWord.insert(contentsOf: reverseResult, at: myWord.endIndex)
+
+print(myWord)
+
+reverseResult.append(" and good morning")
+print(reverseResult)
+
+//remove at
+myWord.remove(at: myWord.index(myWord.startIndex, offsetBy: 5))
+
+print(myWord)
+
+//create a index range
+let range = myWord.startIndex..<myWord.index(myWord.startIndex, offsetBy: 6)
+
+//use .removeSubrange() method
+myWord.removeSubrange(range)
+
+print(myWord)
+
+//:## Substrings
+
+//substrings obtained by subscripting, or method like prefix, need to be cast to a
+//String for long term storage and are for temporary use
+print(myWord[range])
+
+let strMessage = "The night is young"
+let index = strMessage.firstIndex(of: " ") ?? strMessage.endIndex
+let firstWordInStr = strMessage[..<index]
+print(firstWordInStr)
+//or
+let strSlice = strMessage[strMessage.startIndex..<strMessage.index(strMessage.startIndex, offsetBy: 4)]
+print(strSlice)
+
+//converting the substring to string
+let permanentFirstWord = String(firstWordInStr)
+
+//substring is a range of the base string and reuses the memrory space of the parent
 
 
+//:## Comparing Strings
+
+//:### String and Character Equality
+
+//use == and !=
+let compareStr1 = "Hello everyone"
+let compareStr2 = "Hello everyone"
+
+//need to have the same linguistic meaning and appearance
+if compareStr1 == compareStr2 {
+    print("These are equal strings")
+    
+}
+
+//:## Prefix and Suffix Equality
+
+//use the hasPrefix() and hasSuffix() methods to check if string contains passed in
+//string argument
+
+let phoneNum = ["909-355-4353","909-399-5555","909-888-5555"]
+
+var countResult = 0
+var countResult2 = 0
+for num in phoneNum{
+    if num.hasPrefix("909") {
+        countResult += 1
+    }
+}
+print("There are \(countResult) 909 numbers")
+for num in phoneNum{
+    if num.hasSuffix("5555") {
+        countResult2 += 1
+    }
+}
+print("There are \(countResult2) 5555 numbers")
