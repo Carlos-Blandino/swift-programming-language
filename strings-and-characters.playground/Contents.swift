c33e621af91aebc52dd4de85d674501117b9deee
Carlos Blandino <cablandino66@gmail.com>
2020-05-07 13:50:05 -0700

    adding unicode, scalar values, and extended grapheme cluster

--- strings-and-characters.playground/Contents.swift
+++ strings-and-characters.playground/Contents.swift
@@ -119,6 +119,22 @@ print("\(pi) does not change")
 //use extended string delimiters to show the value within the delimiters
 print(#"\(pi) does not change"#)
 
+//:## Unicode
+
+//:### Unicode Scalar Values
+
+//Swifts native String type is built from Unicode scalar values and
+//are Unicode-compliant
+
+//:### Extended Grapheme Clusters
+
+//this set the stage for why we index a string with String.index type rather than
+//an Integer
+
+//each Character type represents a single "extended grapheme cluster" which in certain
+//instances the characters can be combined, forming a single human-readable character
+//and count as one - many combined memory spaces will be represented by the String.index representaion
+
 //:## Counting Characters
 
 var resultCount = mySampleDelimitedStr.count
@@ -129,9 +145,26 @@ schoolString += japaneseYen
 print(schoolString.count)
 
 var myWord = "resume"
+//every character is a single "extended grapheme cluster"
+//the last letter "e" will be combined with another single "extended grapheme cluster"
+//creating a cluster of two scalars
 myWord += "\u{301}"
 print(myWord)
 //string's character count not affected in this instance
 print(myWord.count)
+
+//:## Accessing and Modifying a String
+
+//:### String Indices
+
+//because of the possibility of combined cluster(memory requirements can differ), strings are
+//not indexed by an integer but rather the type is a String.index
+//for each position of each character in the string
+
+//you must iterate over each Unicode scalar from the .startIndex or .endIndex of the
+//string,
+
+//startIndex and endIndex are equal when the string is empty
+
 
 
