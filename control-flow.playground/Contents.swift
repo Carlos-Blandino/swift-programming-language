40a44997052031f50cc90522e5d88ad336c9761a
Carlos Blandino <cablandino66@gmail.com>
2020-05-11 22:25:57 -0700

    working on guard statement example

--- control-flow.playground/Contents.swift
+++ control-flow.playground/Contents.swift
@@ -124,3 +124,155 @@ default:
     print("The choosen color is not a primary color")
 }
 
+//:### Interval Matching
+
+let characters = "a"
+switch characters {
+case "a"..."z":
+    print("You choose a lower case letter")
+case "A"..."Z":
+    print("You choose a upper case letter")
+default:
+    print("You did not choose a letter")
+}
+
+//:### Tuples
+//Use an (_) underscore as a wild card in a tuple when used to test
+//multiple values in a switch statement
+
+let somePoint = (2, 1)
+switch somePoint {
+case (0, 0):
+    print("\(somePoint) is the origin")
+case (_, 0):
+    print("\(somePoint) along the x-axis")
+case (0, _):
+    print("\(somePoint) along the y-axis")
+case (-3...3, -3...3):
+    print("\(somePoint) is inside the the 3 x 3 box")
+default:
+    print("\(somePoint) is outside of the 3 x 3 box")
+}
+
+//:### Value Binding
+
+//instead of using an (_) an underscore, which basically its
+//saying unmaned variable goes in here, you can declare a var/let
+//variable to allow usage within the case statement
+
+let anotherPoint = (2, 1)
+switch anotherPoint {
+case (let x, 0):
+    print("on the x-axis with an x value of \(x)")
+case (0, let y):
+    print("on the y-axis with a y value of \(y)")
+case let (x, y):
+    print("somewhere else at (\(x), \(y))")
+}
+
+//use cane use a (where) clause for extra checks
+//checking the points of diagnal lines in cartesian
+//coordinate system
+
+let yetAnotherPoint = (1, -1)
+switch yetAnotherPoint {
+case let (x, y) where x == y:
+    print("(\(x), \(y)) is on the line x == y")
+case let (x, y) where x == -y:
+    print("(\(x), \(y)) is on the line x == -y")
+case let (x, y):
+    print("(\(x), \(y)) is just some arbitrary point")
+}
+
+//:### Compound Cases
+
+let someCharacter: Character = "e"
+switch someCharacter {
+case "a", "e", "i", "o", "u":
+    print("\(someCharacter) is a vowel")
+case "b", "c", "d", "f", "g", "h", "j", "k", "l", "m",
+     "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z":
+    print("\(someCharacter) is a consonant")
+default:
+    print("\(someCharacter) is not a vowel or a consonant")
+}
+
+//:### Compound Cases w/ Value Binding
+
+let stillAnotherPoint = (9, 0)
+switch stillAnotherPoint {
+case (let distance, 0), (0, let distance):
+    print("On an axis, \(distance) from the origin")
+default:
+    print("Not on an axis")
+}
+
+//:## Control Transfer Statements
+
+//:## Continue
+//stops the current iteration and tells it to start
+//on the next iteration
+let puzzleInput = "great minds think alike"
+var puzzleOutput = ""
+let charactersToRemove: [Character] = ["a", "e", "i", "o", "u", " "]
+for character in puzzleInput {
+    if charactersToRemove.contains(character) {
+        continue
+    }
+    puzzleOutput.append(character)
+}
+print(puzzleOutput)
+
+//:## Break in Loops and Switch statements
+//Terminates the switch or loop statements execution
+//and continues on to the next statement after the closing
+//brace
+
+//:## Fallthrough
+//Typically a switch statement would fallthrough and would require
+//a break statement but in swift we need to use a fallthrough
+//control tarnsfer statement to explicity make it fallthrough
+switch choosenColor{
+case "red":
+    //print("The choosen color is \(choosenColor)")
+    fallthrough
+case "blue":
+    //print("The choosen color is \(choosenColor)")
+    fallthrough
+case "yellow":
+    print("The choosen color is \(choosenColor)")
+    print("You have choosen one of the primary colors")
+default:
+    print("The choosen color is not a primary color")
+}
+
+
+//:### Labeled Statements
+
+//to clarify the application of a control transfer statement
+//use labels on the loops and conditional statements
+    var x = 5
+    var y = 0
+ nameOfLoop: while x>y {
+    y += 1
+    switch y {
+    case 1:
+        print(y)
+    case 2:
+        print(y)
+    case 3:
+        print("printing \(y) before iterating over again")
+        continue nameOfLoop
+    case 4:
+        break nameOfLoop
+    default:
+        break
+    }
+    print("end of switch")
+   }
+print("end of while")
+
+//:### Return - Early Exit
+
+//guard statement used for optional binding
+
