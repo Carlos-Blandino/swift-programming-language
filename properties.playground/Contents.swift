9ae2c2bdf967f8f78380912a6fd051cfcdd02606
Carlos Blandino <cablandino66@gmail.com>
2020-05-30 12:45:24 -0700

    completing properties page

--- properties.playground/Contents.swift
+++ properties.playground/Contents.swift
@@ -44,4 +44,460 @@ let rangeOfFourItems = FixedLengthRange(firstValue: 0, length: 4)
  The initial  value is not calculated until the first time it is used
  Only use with var
  
+ Use lazy modifier with var when:
+ 1. when the initial value of a property is dependent on outside
+ factors whose values are not know until after an instance's initialization is complete
+ 2. when the initial value for a property requires complex or computationally expensive setup that should not be
+ performed unless / until needed
  */
+
+class DataImporter {
+    /*
+    DataImporter is a class to import data from an external file.
+    The class is assumed to take a nontrivial amount of time to initialize.
+    */
+    var filename = "data.txt"
+    // the DataImporter class would provide data importing functionality here
+}
+
+class DataManager {
+    lazy var importer = DataImporter()
+    var data = [String]()
+    // the DataManager class would provide data management functionality here
+}
+
+let manager = DataManager()
+manager.data.append("Some data")
+manager.data.append("Some more data")
+// the DataImporter instance for the importer property has not yet been created
+
+print(manager.importer.filename)
+// the DataImporter instance for the importer property has now been created
+// Prints "data.txt"
+
+//NOTE
+
+/*:
+ If a property marked with the lazy modifier is accessed by multiple threads simultaneously and the property has not yet been initialized, there is no guarantee that the property will be initialized only once.
+*/
+
+//:## Computed Properties - (structures, classes, and enumerations)
+
+struct Point {
+    var x = 0.0, y = 0.0
+}
+struct Size {
+    var width = 0.0, height = 0.0
+}
+struct Rect {
+    var origin = Point()
+    var size = Size()
+    var center: Point {
+        get {
+            let centerX = origin.x + (size.width / 2)
+            let centerY = origin.y + (size.height / 2)
+            return Point(x: centerX, y: centerY)
+        }
+        set(newCenter) {
+            origin.x = newCenter.x - (size.width / 2)
+            origin.y = newCenter.y - (size.height / 2)
+        }
+    }
+}
+var square = Rect(origin: Point(x: 0.0, y: 0.0),
+                  size: Size(width: 10.0, height: 10.0))
+let initialSquareCenter = square.center
+square.center = Point(x: 15.0, y: 15.0)
+print("square.origin is now at (\(square.origin.x), \(square.origin.y))")
+// Prints "square.origin is now at (10.0, 10.0)"
+
+//:### Shorthand Setter Declaration
+//newValue is used as a default when no name is given
+struct AlternativeRect {
+    var origin = Point()
+    var size = Size()
+    var center: Point {
+        get {
+            let centerX = origin.x + (size.width / 2)
+            let centerY = origin.y + (size.height / 2)
+            return Point(x: centerX, y: centerY)
+        }
+        set {
+            origin.x = newValue.x - (size.width / 2)
+            origin.y = newValue.y - (size.height / 2)
+        }
+    }
+}
+
+//:### Shorthand Getter Declaration
+//for single expression getter body
+struct CompactRect {
+    var origin = Point()
+    var size = Size()
+    var center: Point {
+        get {
+            Point(x: origin.x + (size.width / 2),
+                  y: origin.y + (size.height / 2))
+        }
+        set {
+            origin.x = newValue.x - (size.width / 2)
+            origin.y = newValue.y - (size.height / 2)
+        }
+    }
+}
+
+
+//:### Read-Only Computed Properties
+
+struct Cuboid {
+    var width = 0.0, height = 0.0, depth = 0.0
+    var volume: Double {
+        return width * height * depth
+    }
+}
+let fourByFiveByTwo = Cuboid(width: 4.0, height: 5.0, depth: 2.0)
+print("the volume of fourByFiveByTwo is \(fourByFiveByTwo.volume)")
+// Prints "the volume of fourByFiveByTwo is 40.0"
+
+//:## Property Observers
+//can't use on lazy stored properties
+
+/*:
+ You have the option to define either or both of these observers on a property:
+
+ willSet is called just before the value is stored.
+ didSet is called immediately after the new value is stored.
+ 
+  WillSet: If you don’t write the parameter name and parentheses within your implementation, the parameter is made available with a default parameter name of newValue
+ 
+ didSet: You can name the parameter or use the default parameter name of oldValue.
+ */
+
+class StepCounter {
+    var totalSteps: Int = 0 {
+        willSet(newTotalSteps) {
+            print("About to set totalSteps to \(newTotalSteps)")
+        }
+        didSet {
+            if totalSteps > oldValue  {
+                print("Added \(totalSteps - oldValue) steps")
+            }
+        }
+    }
+}
+let stepCounter = StepCounter()
+stepCounter.totalSteps = 200
+// About to set totalSteps to 200
+// Added 200 steps
+stepCounter.totalSteps = 360
+// About to set totalSteps to 360
+// Added 160 steps
+stepCounter.totalSteps = 896
+// About to set totalSteps to 896
+// Added 536 steps
+
+
+//:## Property Wrappers
+/*:
+ When you use a property wrapper, you write the management code once
+ when you define the wrapper, and then reuse that management code by
+ applying it to multiple properties
+ 
+ 1. create a structure, enumeration or class that defines  a "var wrappedValue"
+ 
+ NOTE
+
+ The declaration for number is marked as private,  ensures number is used only in the implementation of TwelveOrLess. No direct access outside the struct.
+ */
+//define the property wrapper and create a var wrappedValue
+
+@propertyWrapper
+struct TwelveOrLess {
+    private var number: Int
+    init() { self.number = 0 }
+    var wrappedValue: Int {
+        get { return number }
+        set { number = min(newValue, 12) }
+    }
+
+}
+//apply a wrapper to a property by writing the wrapper’s name before the property as an attribute
+
+struct SmallRectangle1 {
+    @TwelveOrLess var height: Int
+    @TwelveOrLess var width: Int
+}
+
+var rectangle = SmallRectangle1()
+print(rectangle.height)
+// Prints "0"
+
+rectangle.height = 10
+print(rectangle.height)
+// Prints "10"
+
+rectangle.height = 24
+print(rectangle.height)
+// Prints "12"
+
+
+//You can also use the property wrapper with out the attribute syntax as follows:
+
+struct SmallRectangle2 {
+    private var _height = TwelveOrLess()
+    private var _width = TwelveOrLess()
+    var height: Int {
+        get { return _height.wrappedValue }
+        set { _height.wrappedValue = newValue }
+    }
+    var width: Int {
+        get { return _width.wrappedValue }
+        set { _width.wrappedValue = newValue }
+    }
+}
+
+//:### Setting Initial Values for Wrapped Poperties
+
+@propertyWrapper
+struct SmallNumber {
+    private var maximum: Int
+    private var number: Int
+
+    var wrappedValue: Int {
+        get { return number }
+        set { number = min(newValue, maximum) }
+    }
+
+    init() {
+        maximum = 12
+        number = 0
+    }
+    init(wrappedValue: Int) {
+        maximum = 12
+        number = min(wrappedValue, maximum)
+    }
+    init(wrappedValue: Int, maximum: Int) {
+        self.maximum = maximum
+        number = min(wrappedValue, maximum)
+    }
+}
+
+//case 0
+struct ZeroRectangle {
+    @SmallNumber var height: Int
+    @SmallNumber var width: Int
+}
+
+var zeroRectangle = ZeroRectangle()
+print(zeroRectangle.height, zeroRectangle.width)
+// Prints "0 0"
+
+
+//case 1
+struct UnitRectangle {
+    @SmallNumber var height: Int = 1
+    @SmallNumber var width: Int = 1
+}
+
+var unitRectangle = UnitRectangle()
+print(unitRectangle.height, unitRectangle.width)
+// Prints "1 1"
+
+//case 2
+struct NarrowRectangle {
+    @SmallNumber(wrappedValue: 2, maximum: 5) var height: Int
+    @SmallNumber(wrappedValue: 3, maximum: 4) var width: Int
+}
+
+var narrowRectangle = NarrowRectangle()
+print(narrowRectangle.height, narrowRectangle.width)
+// Prints "2 3"
+
+narrowRectangle.height = 100
+narrowRectangle.width = 100
+print(narrowRectangle.height, narrowRectangle.width)
+// Prints "5 4"
+
+
+//case 3
+struct MixedRectangle {
+    @SmallNumber var height: Int = 1
+    @SmallNumber(maximum: 9) var width: Int = 2
+}
+
+var mixedRectangle = MixedRectangle()
+print(mixedRectangle.height)
+// Prints "1"
+
+mixedRectangle.height = 20
+print(mixedRectangle.height)
+// Prints "12"
+
+
+//:### Projecting a Value From a Property Wrapper
+
+//keeps track true or flase wether property wrapper made adjustments the newValue
+//1. define a projectedValue
+//2. The name of the projected value is the same as the wrapped value - note: it begins with $
+@propertyWrapper
+struct SmallNumber1 {
+    private var number: Int
+    var projectedValue: Bool
+    init() {
+        self.number = 0
+        self.projectedValue = false
+    }
+    var wrappedValue: Int {
+        get { return number }
+        set {
+            if newValue > 12 {
+                number = 12
+                projectedValue = true
+            } else {
+                number = newValue
+                projectedValue = false
+            }
+        }
+    }
+}
+struct SomeStructure {
+    @SmallNumber1 var someNumber: Int
+}
+var someStructure = SomeStructure()
+
+someStructure.someNumber = 4
+print(someStructure.$someNumber)
+// Prints "false"
+
+someStructure.someNumber = 55
+print(someStructure.$someNumber)
+// Prints "true"
+
+
+
+enum Size1 {
+    case small, large
+}
+
+struct SizedRectangle {
+    @SmallNumber1 var height: Int
+    @SmallNumber1 var width: Int
+
+    mutating func resize(to size: Size1) -> Bool {
+        switch size {
+        case .small:
+            height = 10
+            width = 20
+        case .large:
+            height = 100
+            width = 100
+        }
+        return $height || $width
+    }
+}
+
+/*:
+ When you access a projected value from code that’s part of the type, like a property getter or an instance method, you can omit self. before the property name, just like accessing other properties. The code in the following example refers to the projected value of the wrapper around height and width as $height and $width:
+*/
+
+enum Size2 {
+    case small, large
+}
+
+struct SizedRectangle1 {
+    @SmallNumber1 var height: Int
+    @SmallNumber1 var width: Int
+
+    mutating func resize(to size: Size2) -> Bool {
+        switch size {
+        case .small:
+            height = 10
+            width = 20
+        case .large:
+            height = 100
+            width = 100
+        }
+        return $height || $width
+    }
+}
+
+//:## Type Properties
+
+/*:
+ You can also define properties that belong to the type itself, not to any one instance of that type. There will only ever be one copy of these properties, no matter how many instances of that type you create. These kinds of properties are called type properties.
+ 
+ For defining values that are universal to all instances of a particular type, such as a constant property or a variable property that stores a value that is global to all instances.
+ 
+ 1. You must always give stored type properties a default value. There is no initializer for it.
+ 2. They are lazily initialized on their first access. Does not need the lazy modifier
+ */
+
+struct SomeStructure1 {
+    static var storedTypeProperty = "Some value."
+    static var computedTypeProperty: Int {
+        return 1
+    }
+}
+enum SomeEnumeration {
+    static var storedTypeProperty = "Some value."
+    static var computedTypeProperty: Int {
+        return 6
+    }
+}
+class SomeClass {
+    static var storedTypeProperty = "Some value."
+    static var computedTypeProperty: Int {
+        return 27
+    }
+    class var overrideableComputedTypeProperty: Int {
+        return 107
+    }
+}
+
+//:### Querying and Setting Type Properties
+
+print(SomeStructure1.storedTypeProperty)
+// Prints "Some value."
+SomeStructure1.storedTypeProperty = "Another value."
+print(SomeStructure1.storedTypeProperty)
+// Prints "Another value."
+print(SomeEnumeration.computedTypeProperty)
+// Prints "6"
+print(SomeClass.computedTypeProperty)
+// Prints "27"
+
+
+//example
+
+struct AudioChannel {
+    static let thresholdLevel = 10
+    static var maxInputLevelForAllChannels = 0
+    var currentLevel: Int = 0 {
+        didSet {
+            if currentLevel > AudioChannel.thresholdLevel {
+                // cap the new audio level to the threshold level
+                currentLevel = AudioChannel.thresholdLevel
+            }
+            if currentLevel > AudioChannel.maxInputLevelForAllChannels {
+                // store this as the new overall maximum input level
+                AudioChannel.maxInputLevelForAllChannels = currentLevel
+            }
+        }
+    }
+}
+
+var leftChannel = AudioChannel()
+var rightChannel = AudioChannel()
+
+leftChannel.currentLevel = 7
+print(leftChannel.currentLevel)
+// Prints "7"
+print(AudioChannel.maxInputLevelForAllChannels)
+// Prints "7"
+
+
+rightChannel.currentLevel = 8
+print(rightChannel.currentLevel)
+// Prints "8"
+print(AudioChannel.maxInputLevelForAllChannels)
+// Prints "8"
