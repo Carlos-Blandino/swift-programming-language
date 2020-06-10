4f2cc24068e19042ff7e3a0b63c8c29e4832c0c3
Carlos Blandino <cablandino66@gmail.com>
2020-06-09 17:08:32 -0700

    Working on inheritance page

---
+++ Inheritance.playground/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+//:# Inheritance
+
+/*:
+ A class can inherit from another class. The subclass is the class that inheriting from another
+ also known as a superclass.
+ 
+ The subclass has access and can override the superclass properties, methods, subscripts, giving
+ them new behavior.
+ 
+ Classes can also add property observers to inherited properties in order to be notified of value changes.
+ 
+ Property observers can be added to any property ( stored or computed)
+ */
+
